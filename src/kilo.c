0353b18da0bdd47c153346bd6cdbfecbc9ae1088
solisares <rsg.elias@gmail.com>
2025-03-12 21:36:41 +0000

    Added append buffer (step 38)

--- src/kilo.c
+++ src/kilo.c
@@ -79,7 +79,7 @@ int getCursorPosition (int* rows, int* cols) {
         i++;
     }
     buf[i] = '\0';
-    
+
     if (buf[0] != '\x1b' || buf[1] != '[') return -1;
     if (sscanf(&buf[2], "%d;%d, rows, cols") != 2) return -1;
 
@@ -104,36 +104,48 @@ int getWindowSize (int* rows, int* cols) {
 struct abuf {
     char* b;
     int len;
-}
+};
 
 #define ABUF_INIT {NULL, 0}
 
-void abAppend(struc abuf *ab, const char *s, int len) {
+void abAppend(struct abuf *ab, const char *s, int len) {
     char* new = realloc(ab->b, ab->len + len);
 
     if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
 }
 
 /* output */
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
 
         if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2)
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
 
-    editorDrawRows();
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /* input */
